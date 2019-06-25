c2f73f4df9ac32a8936f542226bac382b8af5891
Lukas Bombach <lukas.bombach@googlemail.com>
2019-06-25 09:16:59 +0200

    mediocre types solution

--- lib/de1/converters/index.ts
+++ lib/de1/converters/index.ts
@@ -1,9 +1,13 @@
-import StateConverter from "./state";
+import StateConverter, { State } from "./state";
 
 export { State } from "./state";
 
-export type Keys = keyof Converters;
-export type Values<Key extends Keys> = Converters[Key];
+export type Keys = keyof ConverterTypes;
+export type Values<Key extends Keys> = ConverterTypes[Key];
+
+interface ConverterTypes {
+  state: State;
+}
 
 export interface Converters {
   state: StateConverter;
